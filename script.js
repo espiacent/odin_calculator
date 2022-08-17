b344c529b60e4e1a6d87e587f0842e0f6c9445c3
espiacent <ch.gripp@posteo.ch>
2022-08-17 14:09:22 +0200

    add get input by click and key

--- script.js
+++ script.js
@@ -35,3 +35,46 @@ function Op(op, num1, num2) {
         return Neg(num1)
     }
 }
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => button.addEventListener('click', getButton));
+
+document.addEventListener('keydown', getKey);
+
+function getKey(e) {
+    const numberkeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    if (e.key in numberkeys) {
+        return e.key
+    } else {
+        if (e.key == 'Backspace' || 'c') {
+            return ('clear');
+        }
+        if (e.key == '/') {
+            return ('divide');
+        }
+        if (e.key == '%') {
+            return ('percent');
+        }
+        if (e.key == '+') {
+            return ('plus');
+        }
+        if (e.key == '-') {
+            return ('minus');
+        }
+        if (e.key == '*') {
+            return ('mult');
+        }
+        if (e.key == '=') {
+            return ('equals');
+        }
+        if (e.key == ',') {
+            return ('comma');
+        }
+    }
+}
+
+function getButton(e) {
+    const position = (document.elementFromPoint(e.clientX, e.clientY));
+    const button = position.id;
+    return button
+}
