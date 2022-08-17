1968d41a004f0ee558508ff86c338a4a19652994
espiacent <ch.gripp@posteo.ch>
2022-08-17 14:45:21 +0200

    hook keydown and click to display field

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// global variables
+window.output = '';
+window.op = '';
+window.num1 = '';
+window.num2 = '';
+
+// main operate function
+function Op(op, num1, num2) {
+    if (op == 'plus') {
+        return Add(num1, num2);
+    }
+    if (op == 'minus') {
+        return Sub(num1, num2);
+    }
+    if (op == 'mult') {
+        return Mult(num1, num2);
+    }
+    if (op == "divide") {
+        return Div(num1, num2);
+    }
+    if (op == "neg") {
+        return Neg(num1)
+    }
+    if (op == "clear") {
+        return Clear()
+    }
+}
+
+// calculation functions
 function Add(num1, num2) {
     return num1 + num2;
 }
@@ -18,36 +47,19 @@ function Neg(num1) {
     return (num1 * -1);
 }
 
-function Op(op, num1, num2) {
-    if (op == '+') {
-        return Add(num1, num2);
-    }
-    if (op == '-') {
-        return Sub(num1, num2);
-    }
-    if (op == '*') {
-        return Mult(num1, num2);
-    }
-    if (op == "/") {
-        return Div(num1, num2);
-    }
-    if (op == "--") {
-        return Neg(num1)
-    }
-}
-
+// event listeners
 const buttons = Array.from(document.querySelectorAll('.button'));
 buttons.forEach(button => button.addEventListener('click', getButton));
-
 document.addEventListener('keydown', getKey);
 
+//input functions
 function getKey(e) {
     const numberkeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     if (e.key in numberkeys) {
-        return e.key
+        return fillDisplay(e.key);
     } else {
         if (e.key == 'Backspace' || 'c') {
-            return ('clear');
+            window.output = '0';
         }
         if (e.key == '/') {
             return ('divide');
@@ -76,5 +88,14 @@ function getKey(e) {
 function getButton(e) {
     const position = (document.elementFromPoint(e.clientX, e.clientY));
     const button = position.id;
-    return button
+    return fillDisplay(position.id);
+}
+
+function fillDisplay(value) {
+    const onlyInclude = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    if (value in onlyInclude) {
+        window.output = `${value}`;
+        const display = document.querySelector('.output');
+        display.textContent = `${window.output}`;
+    } else return;
 }
