46b3fa8a1e48caca1d645971b06bf441a2d18809
espiacent <ch.gripp@posteo.ch>
2022-08-20 17:45:54 +0200

    input fix and dot allowed

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // global variables
-window.currentValue = '';
-window.cachedValue = '';
+window.displayValue = '0';
+window.currentResult = '';
+window.cachedResult = '';
+window.firstValue = '';
+window.secondValue = '';
 window.operator = '';
-// window.secondOperation = false;
+window.dotApplied = false;
 
 // event listeners
 const buttons = Array.from(document.querySelectorAll('.button'));
@@ -20,7 +23,7 @@ function getClick(e) {
     // define arrays to get keys (in groups) 
     const numbKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const calcKeys = ['plus', 'minus', 'mult', 'divide', 'equals'];
-    const otherKeys = ['percent', 'allclear', 'clear', 'neg', 'comma'];
+    const otherKeys = ['allclear', 'clear', 'neg', 'dot'];
     // define arrays for check input
     const position = (document.elementFromPoint(e.clientX, e.clientY));
     const buttonValue = position.id;
@@ -32,12 +35,13 @@ function getClick(e) {
             case 'plus':
                 console.log('plus')
                 window.operator = 'plus';
-                window.cachedValue = window.currentValue;
-                window.currentValue = '';
+                break;
+            case 'minus':
+                console.log('minus')
+                window.operator = 'minus';
                 break;
             case 'equals':
                 console.log('equals')
-                mainOperator(window.operator, window.cachedValue, window.currentValue);
                 break;
             default:
                 return;
@@ -48,8 +52,20 @@ function getClick(e) {
             case 'allclear':
                 clearAll();
                 break;
-            case 'comma':
+            case 'clear':
+                clearLast();
                 break;
+            case 'dot':
+                if (window.dotApplied == false) {
+                    console.log(buttonValue)
+                    window.displayValue += '.';
+                    const display = document.querySelector('.output');
+                    display.textContent = `${window.displayValue}`;
+                    window.dotApplied = true;
+                    break;
+                } else {
+                    break;
+                }
             default:
                 return;
         }
@@ -59,42 +75,76 @@ function getClick(e) {
 // output field function
 function fillDisplayNum(buttonValue) {
     // add value to global number variable
-    window.currentValue += buttonValue;
+    window.displayValue += buttonValue;
     // limit number length in output field so there is no overlay
-    let checkLength = window.currentValue;
+    let checkLength = window.displayValue;
     if (checkLength.length > 10) {
         return;
     }
     // check for double leading zero and remove if necessary
-    let floatNum = parseFloat(window.currentValue);
+    let floatNum = parseFloat(window.displayValue);
     if (floatNum % 1 == 0) {
         floatNum.toFixed(0);
-        window.currentValue = floatNum;
+        window.displayValue = floatNum;
     } else {
         floatNum.toFixed(1);
-        window.currentValue = floatNum;
+        window.displayValue = floatNum;
     }
     // new global variable to output
     const display = document.querySelector('.output');
-    display.textContent = `${window.currentValue}`;
+    display.textContent = `${window.displayValue}`;
 }
 
 // function to clear output field
 function clearAll() {
-    console.log('clearing')
+    console.log('clearing all')
     // clear all global variables
-    window.currentValue = '0';
+    window.displayValue = '0';
+    window.currentResult = '';
+    window.cachedResult = '';
     window.cachedValue = '';
+    window.currentValue = '';
     window.operator = '';
+    window.dotApplied = false;
     //clear output field
     const display = document.querySelector('.output');
-    display.textContent = `${window.currentValue}`;
+    display.textContent = `${window.displayValue}`;
+}
+
+function clearLast() {
+    console.log('clearing last')
+    // clear recent global variables
+    window.currentValue = '';
+    window.displayValue = '0';
+    //clear output field
+    const display = document.querySelector('.output');
+    display.textContent = `${window.displayValue}`;
 }
 
 function mainOperator(op, num1, num2) {
     if (op == 'plus') {
         console.log('operation plus')
+        window.dotApplied = false;
         Add(num1, num2);
+    }
+    if (op == 'minus') {
+        console.log('operation minus')
+        window.dotApplied = false;
+        Sub(num1, num2);
+    }
+    if (op == 'mult') {
+        console.log('operation mult')
+        window.dotApplied = false;
+        Mult(num1, num2);
+    }
+    if (op == 'divide') {
+        console.log('operation divide')
+        window.dotApplied = false;
+        Div(num1, num2);
+    }
+    if (op == 'neg') {
+        console.log('operation negative')
+        Neg(num);
     }
 }
 
@@ -111,17 +161,31 @@ function Add(num1, num2) {
 }
 
 function Sub(num1, num2) {
-    return num1 - num2;
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    sum = num1 - num2;
+    return;
 }
 
 function Mult(num1, num2) {
-    return num1 * num2;
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    sum = num1 * num2;
+    result = sum.toFixed();
+    return;
 }
 
 function Div(num1, num2) {
-    return num1 / num2;
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    sum = num1 / num2;
+    result = sum.toFixed();
+    return;
 }
 
 function Neg(num1) {
-    return (num1 * -1);
+    num1 = parseFloat(num)
+    sum = num1 * -1;
+    result = sum.toFixed();
+    return;
 }
